eee9e9a37d780afaf73d06a16913a545b649f47f
hshine1226 <hshine1226@gmail.com>
2020-05-09 14:04:37 +0900

    기능: 추출해온 페이지 번호를 이용해 각각의 페이지를 requests 하는 기능 추가.

--- indeed.py
+++ indeed.py
@@ -1,16 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-INDEED_URL = "https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit=50&radius=25"
+LIMIT = 50
+URL = f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit={LIMIT}"
+
 
 def extract_indeed_pages():
-    result = requests.get(INDEED_URL)
+    result = requests.get(URL)
+
     soup = BeautifulSoup(result.text, "html.parser")
-
-
-    pagination = soup.find('div', class_= 'pagination')
-
-
+    pagination = soup.find('div', class_='pagination')
     links = pagination.find_all('a')
 
     pages = []
@@ -21,3 +20,10 @@ def extract_indeed_pages():
     return max_page
 
 
+def extract_indeed_jobs(last_page):
+    jobs = []
+    for page in range(last_page):
+        result = requests.get(f'{URL}&start={page*LIMIT}')
+        print(result.status_code)
+
+    return jobs
